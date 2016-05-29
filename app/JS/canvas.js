6c45928675f3766a39e6b1309fe112440bd87068
aladin6060 <sivlio.andreas@gmail.com>
2016-05-29 16:32:52 +0200

    verschleider

--- app/JS/canvas.js
+++ app/JS/canvas.js
@@ -1,16 +1,15 @@
 var img = new Image();
 
-// User Variables - customize these to change the image being scrolled, its
-// direction, and the speed.
+
 
 img.src = 'images/Dancing.jpg';
 var CanvasXSize = 800;
 var CanvasYSize = 200;
-var speed = 30; //lower is faster
+var speed = 30; 
 var scale = 1.05;
-var y = -4.5; //vertical offset
+var y = -4.5; 
 
-// Main program
+
 
 var dx = 0.75;
 var imgW;
@@ -23,36 +22,36 @@ var ctx;
 img.onload = function() {
     imgW = img.width*scale;
     imgH = img.height*scale;
-    if (imgW > CanvasXSize) { x = CanvasXSize-imgW; } // image larger than canvas
-    if (imgW > CanvasXSize) { clearX = imgW; } // image larger than canvas
+    if (imgW > CanvasXSize) { x = CanvasXSize-imgW; }
+    if (imgW > CanvasXSize) { clearX = imgW; } 
     else { clearX = CanvasXSize; }
-    if (imgH > CanvasYSize) { clearY = imgH; } // image larger than canvas
+    if (imgH > CanvasYSize) { clearY = imgH; } 
     else { clearY = CanvasYSize; }
-    //Get Canvas Element
+   
     ctx = document.getElementById('canvas').getContext('2d');
-    //Set Refresh Rate
+    
     return setInterval(draw, speed);
 }
 
 function draw() {
-    //Clear Canvas
+
     ctx.clearRect(0,0,clearX,clearY);
-    //If image is <= Canvas Size
+   
     if (imgW <= CanvasXSize) {
-        //reset, start from beginning
+       
         if (x > (CanvasXSize)) { x = 0; }
-        //draw aditional image
+        
         if (x > (CanvasXSize-imgW)) { ctx.drawImage(img,x-CanvasXSize+1,y,imgW,imgH); }
     }
-    //If image is > Canvas Size
+   
     else {
-        //reset, start from beginning
+        
         if (x > (CanvasXSize)) { x = CanvasXSize-imgW; }
-        //draw aditional image
+     
         if (x > (CanvasXSize-imgW)) { ctx.drawImage(img,x-imgW+1,y,imgW,imgH); }
     }
-    //draw image
+   
     ctx.drawImage(img,x,y,imgW,imgH);
-    //amount to move
+  
     x += dx;
 }
